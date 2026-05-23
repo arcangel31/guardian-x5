ec500bd7bc5035870bd2960ad3d59d384058b4f4
arcangel31 <164259188+arcangel31@users.noreply.github.com>
2026-05-23 21:45:50 +0200

    Update service worker cache and assets list

--- sw.js
+++ sw.js
@@ -1,8 +1,24 @@
-const CACHE = 'guardian-x5-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'guardian-x5-v2';
+const ASSETS = [
+  '/guardian-x5/',
+  '/guardian-x5/index.html',
+  '/guardian-x5/manifest.json',
+  '/guardian-x5/icon-192.png',
+  '/guardian-x5/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
